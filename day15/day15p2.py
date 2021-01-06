a716f45cb2d92129386a813580c2208bcab61213
Ghostpupper <9207gusmat@gmail.com>
2021-01-06 20:34:10 +0100

    asdf
    forgot to push this

--- day15/day15p2.py
+++ day15/day15p2.py
@@ -1,4 +1,39 @@
-from adventofcode.input_getter import get_input
+starting_numbers = [1, 3, 2]
+
+def add_number_to_list(n):
+    global starting_numbers
+    last_no = starting_numbers[-1]
+    if last_no not in starting_numbers[:-1]:
+        starting_numbers.append(0)
+        return n
+    else:
+        last_index = find_latest_index(last_no)
+        starting_numbers.remove(last_no)
+        starting_numbers.append(last_index)
+        return n - 1
+
+def find_latest_index(last_no: int, index=0):
+    rev_list = reversed(starting_numbers[:-1])
+    distance = 1
+    for item in rev_list:
+        if item == last_no:
+            return distance
+        distance += 1
+
+def game(n):
+    global starting_numbers
+    while n >= len(starting_numbers):
+        n = add_number_to_list(n)
+    return starting_numbers[n]
+
+def main():
+    num = 30000000
+    print(game(num - 1))
 
 if __name__ == '__main__':
-    in_txt = get_input()
+    list_o_list = [[2,1,3], [1,2,3], [2,3,1], [3,2,1], [3,1,2], [1,20,11,6,12,0]]
+    for o_list in list_o_list:
+        starting_numbers = o_list
+        main()
+        print(starting_numbers)
+
--- day17/day17.py
+++ day17/day17.py
@@ -1,4 +1,38 @@
-from adventofcode.input_getter import get_input
+import re
+
+from input_getter import get_input
+
+a_and_b = r"(\d{1,}) ([+*]) (\d{1,})"
+find_paranth = r"\(([^\)\(]*)\)"
+
+def calc_str(whole_string: str):
+    paranthes = re.findall(find_paranth, whole_string)
+    if paranthes:
+        res = calc_no_panths(paranthes[0])
+        new_str = whole_string.replace(f"({paranthes[0]})", res)
+        return calc_str(new_str)
+    return calc_no_panths(whole_string)
+
+def calc_no_panths(calcul_string):
+    calc_pairs = re.findall(a_and_b, calcul_string)
+    pair = calc_pairs[0]
+    a = int(pair[0])
+    b = int(pair[2])
+    pair_str = f"{pair[0]} {pair[1]} {pair[2]}"
+    c = a + b if pair[1] == '+' else a * b
+    new_calcul_string = calcul_string.replace(pair_str, str(c))
+    if '*' in new_calcul_string or '+' in new_calcul_string:
+        return calc_no_panths(new_calcul_string)
+    return new_calcul_string
+
 
 if __name__ == '__main__':
     in_txt = get_input()
+    all_res = 0
+    ''.isnumeric()
+    for text in in_txt:
+        res = int(calc_str(text))
+        all_res += res
+        print(f"- [{text[:-1] if not text[-1].isnumeric() else text}] becomes: {res}.")
+    print(all_res)
+
